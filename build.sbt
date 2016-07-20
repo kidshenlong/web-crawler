07a1d25e45851d8f0e8200172cfaf83f97cc6b45
kidshenlong <michaelpm91@googlemail.com>
2016-07-20 02:11:17 +0100

    Using Akka HTTP and Jsoup to fetch URL and parse body.

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.8",
+  "com.typesafe.akka" %% "akka-http-core" % "2.4.8",
+  "com.typesafe.akka" %% "akka-http-experimental" % "2.4.8",
   "org.jsoup"         % "jsoup"       % "1.9.2"
 )
--- src/main/scala/mpm/Crawler.scala
+++ src/main/scala/mpm/Crawler.scala
@@ -1,16 +1,30 @@
 package mpm
 
 import java.net.URL
+import java.util.concurrent.Executors
 
-import akka.actor.Actor
+import akka.actor.{ActorRefFactory, Actor}
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import org.jsoup.Jsoup
-
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import org.jsoup.nodes.{Element, Document}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.collection.JavaConverters._
 /**
   * Created by Michael on 20/07/2016.
   */
 class Crawler extends Actor{
 
-  val crawled = scala.collection.mutable.SortedSet[URL]()
+  def actorRefFactory: ActorRefFactory = context
+  implicit val system = context.system
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(200))
+
+
+
+  //val crawled = scala.collection.mutable.SortedSet[URL]()
 
   def receive: Receive = {
     case Start(url) => start(url)
@@ -18,9 +32,29 @@ class Crawler extends Actor{
 
   def start(url: URL): Unit = {
 
+    getLinks(url)
+
   }
 
-  protected def getLinks(url: URL): List[URL] = {
+  protected def getLinks(url: URL) = {//: Future[List[URL]] = {
+
+    //val response = Jsoup.connect()
+
+    val res = Http().singleRequest(HttpRequest(uri = url.toString))//todo (mpm) follow redirects
+
+    res.map( httpResponse => {
+      val futureBody = Unmarshal(httpResponse.entity).to[String]
+      futureBody.map{ body =>
+        val doc = Jsoup.parse(body)
+
+        val elements = doc.select("a").asScala.toList
+
+        elements.map( el => println(el.attr("href")))
+
+
+
+      }
+    })
 
   }
 
